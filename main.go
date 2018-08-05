ad195a9467d9b8c5fee77205dc68ab42372dedd0
ChunkDing <kico0909@gmail.com>
2018-08-05 12:53:12 +0800

    增加启动解析的时间戳日志,最大保留1024byte的日志

--- main.go
+++ main.go
@@ -1,11 +1,19 @@
 package main
 
 import (
-	"log"
-	"ip"
 	"domain"
-	"io/ioutil"
 	"encoding/json"
+	"io/ioutil"
+	"ip"
+	"log"
+	"os"
+	//"time"
+	"time"
+)
+
+const (
+	RunLog        = "./runLog.txt"
+	RunLogMaxSize = 1024
 )
 
 func main() {
@@ -14,6 +22,25 @@ func main() {
 
 	// 创建阿里云解析类
 	domain := domain.New(conf.Appid, conf.Appsecret)
+
+	var f *os.File
+
+	RunLogfileSize, err := os.Stat(RunLog)
+	if err != nil {
+		f, err = os.OpenFile(RunLog, os.O_CREATE|os.O_RDWR, 0777)
+	} else {
+		if RunLogfileSize.Size() > RunLogMaxSize {
+			f, err = os.OpenFile(RunLog, os.O_TRUNC|os.O_RDWR, 0777)
+		} else {
+			f, err = os.OpenFile(RunLog, os.O_RDWR|os.O_APPEND, 0777)
+		}
+	}
+	if err != nil {
+		log.Println(err)
+		os.Exit(99)
+	}
+	defer f.Close()
+	f.WriteString(time.Now().Format("2006-01-02 15:04:05") + "\r\n")
 
 	// 获得外网IP
 	IP := ip.External()
@@ -24,46 +51,45 @@ func main() {
 
 		// 查找指定域名的解析ID
 		recordID := ""
-		for _,v := range res.DomainRecords.Record {
+		for _, v := range res.DomainRecords.Record {
 			if v.RR == info.RR {
 				if v.Value != IP {
 					recordID = v.RecordId
-				}else{
-					log.Println("域名[" + info.RR + "." + info.Domain+"]指向IP未改变,无需重新解析!")
+				} else {
+					//log.Println("域名[" + info.RR + "." + info.Domain+"]指向IP未改变,无需重新解析!")
 					goto STOPTHIS
 				}
 				break
 			}
 		}
 
-		if len(recordID)>0{
+		if len(recordID) > 0 {
 			// 修改域名解析记录
 			if domain.UpdateDomainRecord(info.RR, recordID, IP) {
-				log.Println("[" + info.RR + "." + info.Domain+"] 更新成功!")
-			}else{
-				log.Println("[" + info.RR + "." + info.Domain+"] 更新失败!")
+				//log.Println("[" + info.RR + "." + info.Domain+"] 更新成功!")
+			} else {
+				//log.Println("[" + info.RR + "." + info.Domain+"] 更新失败!")
 			}
-		}else{
+		} else {
 
-			log.Println("[" + info.RR + "." + info.Domain+"] 的解析记录未找到!")
+			//log.Println("[" + info.RR + "." + info.Domain+"] 的解析记录未找到!")
 		}
-		STOPTHIS:
+	STOPTHIS:
 	}
-
-
 
 }
 
 type ConfigType struct {
-	Appid string
+	Appid     string
 	Appsecret string
-	DdnsConf []configTypeChip
+	DdnsConf  []configTypeChip
 }
 type configTypeChip struct {
 	Domain string
-	RR string
+	RR     string
 }
-func configGet()ConfigType{
+
+func configGet() ConfigType {
 	var res ConfigType
 	fc, err := ioutil.ReadFile("./config.json")
 
